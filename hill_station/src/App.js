f1b6e63b0cad4c4de832de450cabc31ab06959f8
aasifwali16@gmail.com <aasifwali16@gmail.com>
2023-08-13 23:17:39 +0530

    Final Push

--- hill_station/src/App.js
+++ hill_station/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import OurWorld from './ProductComponents/OurWorld';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <OurWorld/>
     </div>
   );
 }
